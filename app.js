979db041fb4dd66c3f2ef99269e68547eae29cba
shivam <chotu.kv@gmail.com>
2018-02-07 13:39:27 +0530

    Remove Log

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path 	= require('path');
-const multer  =   require('multer');
+//const multer  =   require('multer');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
@@ -20,18 +20,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGOLAB_URI||"mongodb://ruraldev:ruraldev@ds125578.mlab.com:25578/rural-development");//{useMongoClient: true});
+mongoose.connect("mongodb://ruraldev:ruraldev@ds125578.mlab.com:25578/rural-development");//{useMongoClient: true});
 
 const passport = require('passport');
 
 app.use(passport.initialize());
 app.use(passport.session());
-require('./config/passport')(passport);
+//require('./config/passport')(passport);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 let models_path =  './models';
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -44,6 +44,8 @@ require('./models/User');
 const userModel = mongoose.model('User');
 
 app.get('/register',function(req,res){
+	//var res1 = ;
+	console.log("Res");
 	res.render('register.ejs');
 });
 
@@ -82,28 +84,6 @@ app.get('/successfulregister',function(req,res){
 
 
 
-
-/*
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-*/
 
 app.listen(port,(err)=>{
 	if(!err){
